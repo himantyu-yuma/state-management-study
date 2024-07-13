2631cb9950800cd0eaf78fb3d96d269da579ea5c
yuma-kasahara <yuma.kasahara@dena.com>
2024-07-13 22:39:55 +0900

    add: とりあえずページ表示できるように

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+
+const rootElement = document.getElementById("root")
+if (!rootElement) throw new Error("Failed to find the root Element")
+const root = ReactDOM.createRoot(rootElement)
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
